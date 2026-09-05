e013e9320473e7899baee3d869ce0a5bc5a431ae
Marina Glinskaya <sangriyam@yandex.ru>
2026-09-05 16:49:28 +1100

    Add order cleanup after tests

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -3,6 +3,7 @@ import pytest
 import requests
 
 from data import ORDER_DATA
+from helpers.order_helper import cancel_order
 from urls import ORDERS_URL
 
 
@@ -21,20 +22,26 @@ class TestCreateOrder:
     )
     def test_create_order_with_different_colors(self, color):
         order_data = ORDER_DATA.copy()
+        track = None
 
-        with allure.step("Подготовить данные заказа"):
-            if color is not None:
-                order_data["color"] = color
+        try:
+            with allure.step("Подготовить данные заказа"):
+                if color is not None:
+                    order_data["color"] = color
 
-        with allure.step("Отправить запрос на создание заказа"):
-            response = requests.post(
-                ORDERS_URL,
-                json=order_data
-            )
+            with allure.step("Отправить запрос на создание заказа"):
+                response = requests.post(
+                    ORDERS_URL,
+                    json=order_data
+                )
 
-        response_body = response.json()
+            response_body = response.json()
+            track = response_body.get("track")
 
-        with allure.step("Проверить успешное создание заказа"):
-            assert response.status_code == 201
-            assert "track" in response_body
-            assert isinstance(response_body["track"], int)
+            with allure.step("Проверить успешное создание заказа"):
+                assert response.status_code == 201
+                assert "track" in response_body
+                assert isinstance(response_body["track"], int)
+
+        finally:
+            cancel_order(track)
